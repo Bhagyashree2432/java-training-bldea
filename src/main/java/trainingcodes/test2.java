e90ddd2b50b79116fef1ee174d1f157ef08f0a8c
Bhagyashree <Bhagyashree@Bhagya>
2023-03-20 13:39:41 +0530

    second commit

--- src/main/java/trainingcodes/test2.java
+++ src/main/java/trainingcodes/test2.java
@@ -1,10 +1,10 @@
 package trainingcodes;
 
-public class test1 {
+public class test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello Everyone");
+
 	}
 
 }
